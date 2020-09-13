e843f233216702fdd6d18657b0520a727881e15a
BeauNouvelle <beau@getswifty.dev>
2020-09-13 12:41:15 +1000

    Initial Commit

---
+++ Covid-tracking/Covid_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Covid_trackingApp.swift
+//  Covid-tracking
+//
+//  Created by Beau Nouvelle on 2020-09-13.
+//
+
+import SwiftUI
+
+@main
+struct Covid_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
